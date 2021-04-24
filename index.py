d3f3d4c6aeb1e503133458f739491508538d8d28
Ayaan Kaifullah <ayaanansari7921@outlook.com>
2021-04-25 02:06:13 +0530

    formatted the message

--- index.py
+++ index.py
@@ -1,23 +1,31 @@
 import smtplib 
-# import email.message as EmailMessage
+from email.message import EmailMessage
 
-def sendEmail(recipientEmail, message):
-    # initialize sender
-    senderEmail = "enter_your_email"
-    password = "enter_your_password"
+def sendEmail(recipientEmail, subject, body):
+    # initialize email variables
+    senderEmail = "ur_id"
+    password = "ur_pass"
+    messg = EmailMessage()
+    messg.set_content(body)
+    messg['subject'] = subject  
+    messg['to'] = recipientEmail
+    messg['from'] = senderEmail
+    # server variables
     # creates SMTP session
     server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.ehlo() #<< will talk later
+    # server.ehlo() #<< will talk later
     # start TLS for security
     server.starttls()
     # Authentication
     server.login(senderEmail, password)
     # sending the mail
-    server.sendmail(senderEmail, recipientEmail, message)
+    # server.sendmail(senderEmail, recipientEmail, subject, body)
+    server.send_message(messg)
     # terminating the session
     server.quit()
 
 if __name__=='__main__':
-    reciprecipientEmail = "enter_recipient's_email"
-    message =" someone is calling u in class"
-    sendEmail(reciprecipientEmail, message)
+    reciprecipientEmail = "ur_id_recipient"
+    subject = "class reminder"
+    body = "Someone is calling u in class"
+    sendEmail(reciprecipientEmail, subject, body)
